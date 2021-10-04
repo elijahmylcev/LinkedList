4ed707657633f7915570747599f7561f34479773
Elijah <elijahmylcev@gmail.com>
2021-10-04 23:16:26 +0300

    wrote test Node Class

--- LinkedListNode/Node.test.js
+++ LinkedListNode/Node.test.js
@@ -1,8 +1,22 @@
+const Node = require('./Node');
+const node = new Node();
+
+const test_string = 'Hello'
+
 describe('Node:', () => {
-    test.todo('should point to some data.');
-    test.todo('should a pointer to the next node in the list');
+    test('should point to some data', () => {
+        node(test_string)
+        expect(node.value).toBeTruthy();
+        expect(node.value).toEqual('Hello');
+    });
+
+    test('should a pointer to the next node in the list', () => {
+        expect(node.next).toBeDefined();
+    });
 });
 
 describe('Node.toString():', () => {
-    test.todo('should return a value in a string')
+    test('should return a value in a string', () => {
+        expect(node(test_string).toString()).toEqual('value: Hello, next: null');
+    })
 });

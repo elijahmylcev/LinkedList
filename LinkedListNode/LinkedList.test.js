31a1a5b57082b2e86ed45d9ba23523a6211d4494
Elijah <elijahmylcev@gmail.com>
2021-10-04 21:45:51 +0300

    added specification

---
+++ LinkedListNode/LinkedList.test.js
@@ -0,0 +1,18 @@
+describe('push(Node):', () => {
+    test.todo('inserts an element at the end of the list')
+    test.todo('should return boolean')
+});
+
+describe('insert(Node, index):', () => {
+    test.todo('inserts element at index')
+    test.todo('should return boolean')
+});
+
+describe('get(index):', () => {
+    test.todo('should return the element at the specified index')
+    test.todo('should return null if there is no element with an index')
+});
+
+describe('length:', () => {
+    test.todo('getter, returns the actual length of the list')
+});
---
+++ LinkedListNode/Node.test.js
@@ -0,0 +1,8 @@
+describe('Node:', () => {
+    test.todo('should point to some data.');
+    test.todo('should a pointer to the next node in the list');
+});
+
+describe('Node.toString():', () => {
+    test.todo('should return a value in a string')
+});

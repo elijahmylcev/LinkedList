05143c4fa0280c4520a0b972721f8c8bf467cfb8
Elijah <elijahmylcev@gmail.com>
2021-10-06 21:37:23 +0300

    wrote specification for LinkedList

--- LinkedListNode/LinkedList.test.js
+++ LinkedListNode/LinkedList.test.js
@@ -1,18 +1,61 @@
-describe('push(Node):', () => {
-    test.todo('inserts an element at the end of the list')
-    test.todo('should return boolean')
+const LinkedList = require('./LinkedList');
+let list = new LinkedList();
+
+describe('push(Node), Node(value, next):', () => {
+    beforeEach(() => {
+        node = new Node()
+    });
+    test('inserts an element at the end of the list', () => {
+        list.push('vlaue-test')
+        list.push('end-string')
+        expect(list[list.length - 1].next).toBeNull();
+    })
+    test('should return boolean', () => {
+        expect(list.push('element') === Boolean).toBeTruthy();
+    })
 });
 
 describe('insert(Node, index):', () => {
-    test.todo('inserts element at index')
-    test.todo('should return boolean')
+
+    beforeEach(() => {
+        list.push('el_1')
+        list.push('el_2')
+        list.push('el_3')
+    })
+
+    test('inserts element at index', () => {
+        list.insert('el_test_index=3', 1)
+        expect(list[1] === {value: 'el_test_index=3', next: Object}).toBeTruthy();
+    })
+    test('should return boolean', () => {
+        expect(list.insert('value-test', 1) === Boolean).toBeTruthy();
+    })
 });
 
 describe('get(index):', () => {
-    test.todo('should return the element at the specified index')
-    test.todo('should return null if there is no element with an index')
+
+    beforeEach(() => {
+        list.push('el_1')
+        list.push('el_2')
+        list.push('el_3')
+    })
+    
+    test('should return the element at the specified index', () => {
+        expect(list.get(list.length - 1)).toEqual({
+            value: 'el_3',
+            next: null
+        });
+    })
+    test('should return null if there is no element with an index', () => {
+        expect(list.get(5)).toBeNull();
+    })
 });
 
 describe('length:', () => {
-    test.todo('getter, returns the actual length of the list')
+    test('getter, returns the actual length of the list', () => {
+        list.push('el_0')
+        list.push('el_1')
+        list.push('el_2')
+        expect(list.length).toBe(3);
+    })
 });

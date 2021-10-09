2b6c671ae605574536b6b9c38ca5143d10d8126a
tardigrada777 <ntwrite@gmail.com>
2021-10-09 23:43:48 +0300

    refactor: finish Node class review

--- src/__specs__/Node.test.js
+++ src/__specs__/Node.test.js
@@ -1,27 +1,29 @@
 const Node = require('../Node');
-const test_string = 'Hello'
-let node = new Node();
-
+const testString = 'Hello'
 
 describe('Node:', () => {
 
+  let node = new Node();
+
   beforeEach(() => {
-    node = new Node(test_string)
+    node = new Node(testString)
   });
 
   test('should point to some data', () => {
-    expect(node.value).toBeTruthy();
+    expect(node.value).toBeDefined();
     expect(node.value).toEqual('Hello');
   });
 
   test('should a pointer to the next node in the list', () => {
-    expect(node.next).toBeDefined();
+    expect(node.next).toBeNull();
+    node.next = { value: 1 };
+    expect(node.next.value).toBe(1);
   });
 });
 
 describe('Node.toString():', () => {
   beforeEach(() => {
-    node = new Node(test_string)
+    node = new Node(testString)
   });
 
   test('should return a value in a string', () => {
